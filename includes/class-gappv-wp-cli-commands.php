c4404345ff37a789b6100dd49039f1f45c6db91e
Robert Went <i@robertwent.com>
2023-07-01 23:15:17 +0100

    Formatting

--- includes/class-gappv-wp-cli-commands.php
+++ includes/class-gappv-wp-cli-commands.php
@@ -17,8 +17,8 @@ class Gappv_Wp_Cli_Commands extends WP_CLI_Command {
 		if ( ! class_exists( 'Gappv_Admin' ) ) {
 			WP_CLI::line( __( 'Gappv_Admin Class does not exist!', 'gappv' ) );
 		}
-		$gappv = new Gappv_Admin( 'gappv', GAPPV_VERSION );
-		$args  = array(
+		$gappv           = new Gappv_Admin( 'gappv', GAPPV_VERSION );
+		$args            = array(
 			'post_type'      => 'post',
 			'post_status'    => 'publish',
 			'meta_query'     => array(
@@ -30,8 +30,9 @@ class Gappv_Wp_Cli_Commands extends WP_CLI_Command {
 			'posts_per_page' => 10,
 		);
 		$args['orderby'] = 'date';
-		$args['order'] = 'DESC';
-		$query = new WP_Query( $args );
+		$args['order']   = 'DESC';
+		$query           = new WP_Query( $args );
+		$output          = '';
 		if ( $query->have_posts() ) {
 			while ( $query->have_posts() ) {
 				$query->the_post();
@@ -40,7 +41,7 @@ class Gappv_Wp_Cli_Commands extends WP_CLI_Command {
 				$link      = '/' . $basename . '/';
 				$post_date = get_the_date( 'Y-m-d', $post_id );
 				$views     = $gappv->call_api( $post_id, $link, $post_date );
-				WP_CLI::line( sprintf( __( 'Post Path: %1$s, Views: %2$d', 'gappv' ), $link, $views ) );
+				$output   .= WP_CLI::line( sprintf( __( 'Post Path: %1$s, Views: %2$d', 'gappv' ), $link, $views ) );
 			}
 		}
 		wp_reset_postdata();
